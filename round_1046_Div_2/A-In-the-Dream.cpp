68eb315f7413dbafdb3fcc46a9d1d807f8fe2760
onlyoneflow <105132769+Mandarin07@users.noreply.github.com>
2025-08-31 21:38:38 +0300

    Solution

---
+++ round_1046_Div_2/A-In-the-Dream.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int x, y;
+        cin >> x >> y;
+        // 1 к / 2 -  2 k > 3
+        int z, w;
+        cin >> z >> w;
+
+        if (max(x,y) <= 2*(min(x,y) + 1) && max(z, w) <= 2*(min(z, w) + 1)) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+    }
+}
